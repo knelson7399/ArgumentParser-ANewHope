a70da2fd616025fc6800acb696aac6140bfaf959
cdikoko <cdikoko@stu.jsu.edu>
2017-03-02 03:01:04 -0600

    VolumeCalculatorTest now is ArgumentParserTest

--- build/scripts/ArgumentParser-ANewHope.bat
+++ build/scripts/ArgumentParser-ANewHope.bat
@@ -1,7 +1,7 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  ArgumentParser startup script for Windows
+@rem  ArgumentParser-ANewHope startup script for Windows
 @rem
 @rem ##########################################################################
 
@@ -13,7 +13,7 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%..
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and ARGUMENT_PARSER_OPTS to pass JVM options to this script.
+@rem Add default JVM options here. You can also use JAVA_OPTS and ARGUMENT_PARSER_A_NEW_HOPE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
@@ -63,19 +63,19 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\ArgumentParser-1.0.jar
+set CLASSPATH=%APP_HOME%\lib\ArgumentParser-ANewHope-1.0.jar
 
-@rem Execute ArgumentParser
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %ARGUMENT_PARSER_OPTS%  -classpath "%CLASSPATH%" edu.jsu.mcis.parseArgument %CMD_LINE_ARGS%
+@rem Execute ArgumentParser-ANewHope
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %ARGUMENT_PARSER_A_NEW_HOPE_OPTS%  -classpath "%CLASSPATH%" edu.jsu.mcis.parseArgument %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable ARGUMENT_PARSER_EXIT_CONSOLE if you need the _script_ return code instead of
+rem Set variable ARGUMENT_PARSER_A_NEW_HOPE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if  not "" == "%ARGUMENT_PARSER_EXIT_CONSOLE%" exit 1
+if  not "" == "%ARGUMENT_PARSER_A_NEW_HOPE_EXIT_CONSOLE%" exit 1
 exit /b 1
 
 :mainEnd
